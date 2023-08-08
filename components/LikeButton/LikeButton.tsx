d16209f59bbd97991e2d11070a51b520df9eff06
rawan.abdelkhaleq <rawan.abdelkhaleq@gmail.com>
2023-08-08 14:25:16 +0300

    local storage update

--- components/LikeButton/LikeButton.tsx
+++ components/LikeButton/LikeButton.tsx
@@ -23,35 +23,56 @@ interface LikeButtonProps {
 function LikeButton({object} : LikeButtonProps) {
 
   const [likeState, setLikeState] = useState(false);
+  const [posts, setPosts] = useState<Post[]>([]);
 
   const toggle = () => {
-    setLikeState(!likeState);
-  }
-  
 
-  useEffect(() => {
+    setLikeState(!likeState);
+    
     const tmp = localStorage.getItem('posts');
     let existingArray = tmp ? JSON.parse(tmp) : [];
-
-    if (likeState) {
+  
+    if (!likeState) {
       existingArray.push(object);
       localStorage.setItem("posts", JSON.stringify(existingArray));
+      setPosts(existingArray);
     }
     else{
-      let newArray = [];
-      for(let i=0; i<existingArray.length; i++){
-        if(existingArray[i] != object){
-          
-          if(existingArray[i].name != object.name )newArray.push(existingArray[i]);
-        }
-      }
-      console.log(newArray)
+      let newArray = existingArray.filter(item => item.name !== object.name);;
+      // for(let i=0; i<existingArray.length; i++){
+      //   if(existingArray[i].name != object.name)
+      //     newArray.push(existingArray[i]);
+      // }
       localStorage.setItem("posts", JSON.stringify(newArray));
+      setPosts(newArray);
     }
+  }
 
-  },[likeState])
+  // useEffect(() => {
+  //   const tmp = localStorage.getItem('posts');
+  //   let existingArray = tmp ? JSON.parse(tmp) : [];
 
+  //   if (likeState) {
+  //     existingArray.push(object);
+  //     localStorage.setItem("posts", JSON.stringify(existingArray));
+  //   }
+  //   else{
+  //     let newArray = [];
+  //     for(let i=0; i<existingArray.length; i++){
+  //       if(existingArray[i] != object){
+          
+  //         if(existingArray[i].name != object.name )newArray.push(existingArray[i]);
+  //       }
+  //     }
+  //     localStorage.setItem("posts", JSON.stringify(newArray));
+  //   }
 
+  // },[likeState, object])
+
+  useEffect(()=>{
+    const tmp = localStorage.getItem('posts');
+    setPosts(tmp ? JSON.parse(tmp) : []);
+  },[]);
 
   return (
     <LikeButtonStyle liked={likeState} onClick={toggle}/>
